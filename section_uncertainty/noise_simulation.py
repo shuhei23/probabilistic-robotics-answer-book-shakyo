2a7ffe91a530d2bb249c2474d4440e5768e4e95d
shuhei23 <shuhei.toda@gmail.com>
2023-01-07 15:14:03 +0900

    4.3.4 見落としの実装

--- section_uncertainty/noise_simulation.py
+++ section_uncertainty/noise_simulation.py
@@ -86,7 +86,8 @@ class Camera(IdealCamera):
         direction_range=(-math.pi/3, math.pi/3), 
         distance_noise_rate=0.1, direction_noise=math.pi/90,
         distance_bias_rate_stddev = 0.1, direction_bias_stddev = math.pi/90,
-        phantom_prob = 0.0, phantom_range_x =(-5.0, 5.0), phantom_range_y=(-5.0, 5.0) ): # init()の引数ここまで
+        phantom_prob = 0.0, phantom_range_x =(-5.0, 5.0), phantom_range_y=(-5.0, 5.0),
+        oversight_prob=0.1 ): # init()の引数ここまで
         super().__init__(env_map, distance_range, direction_range) # 元のinitを呼び出す
 
         self.distance_noise_rate = distance_noise_rate
@@ -96,6 +97,7 @@ class Camera(IdealCamera):
         rx, ry = phantom_range_x, phantom_range_y
         self.phantom_dist = uniform(loc=(rx[0], ry[0]), scale = (rx[1]-rx[0], ry[1]-ry[0]))
         self.phantom_prob = phantom_prob
+        self.oversight_prob = oversight_prob
 
     def noise(self, relpos):
         ell = norm.rvs(loc=relpos[0], scale=relpos[0]*self.distance_noise_rate)
@@ -112,11 +114,18 @@ class Camera(IdealCamera):
         else:
             return relpos
 
+    def oversight(self,relpos):
+        if uniform.rvs() < self.oversight_prob:
+            return None
+        else:
+            return relpos
+
     def data(self, cam_pose):
         observed = []
         for lm in self.map.landmarks:
             z = self.observation_function(cam_pose, lm.pos)
             z = self.phantom(cam_pose, z)
+            z = self.oversight(z) # phantom も oversight しうる
             if self.visible(z):
                 z = self.bias(z)
                 z = self.noise(z)
