8472726a3d900efbefe0535aaa76e9b2cf9574e5
shuhei23 <shuhei.toda@gmail.com>
2023-01-07 15:00:22 +0900

    4.3.3 ファントムの実装

--- section_uncertainty/noise_simulation.py
+++ section_uncertainty/noise_simulation.py
@@ -85,13 +85,17 @@ class Camera(IdealCamera):
         distance_range=(0.5, 6.0), 
         direction_range=(-math.pi/3, math.pi/3), 
         distance_noise_rate=0.1, direction_noise=math.pi/90,
-        distance_bias_rate_stddev = 0.1, direction_bias_stddev = math.pi/90):
+        distance_bias_rate_stddev = 0.1, direction_bias_stddev = math.pi/90,
+        phantom_prob = 0.0, phantom_range_x =(-5.0, 5.0), phantom_range_y=(-5.0, 5.0) ): # init()の引数ここまで
         super().__init__(env_map, distance_range, direction_range) # 元のinitを呼び出す
 
         self.distance_noise_rate = distance_noise_rate
         self.direction_noise = direction_noise
         self.distance_bias_rate_std = norm.rvs(scale = distance_bias_rate_stddev)
         self.direction_bias = norm.rvs(scale = direction_bias_stddev)
+        rx, ry = phantom_range_x, phantom_range_y
+        self.phantom_dist = uniform(loc=(rx[0], ry[0]), scale = (rx[1]-rx[0], ry[1]-ry[0]))
+        self.phantom_prob = phantom_prob
 
     def noise(self, relpos):
         ell = norm.rvs(loc=relpos[0], scale=relpos[0]*self.distance_noise_rate)
@@ -101,10 +105,18 @@ class Camera(IdealCamera):
     def bias(self, relpos):
         return relpos + np.array([relpos[0]*self.distance_bias_rate_std, self.direction_bias]).T
     
+    def phantom(self, cam_pose, relpos):
+        if uniform.rvs() < self.phantom_prob:
+            pos_phantom = np.array(self.phantom_dist.rvs()).T
+            return self.observation_function(cam_pose, pos_phantom)
+        else:
+            return relpos
+
     def data(self, cam_pose):
         observed = []
         for lm in self.map.landmarks:
             z = self.observation_function(cam_pose, lm.pos)
+            z = self.phantom(cam_pose, z)
             if self.visible(z):
                 z = self.bias(z)
                 z = self.noise(z)
