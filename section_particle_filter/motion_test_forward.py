0fce2c3d9d2a1891c9863850e30237d06c62eee0
shuhei23 <shuhei.toda@gmail.com>
2023-01-21 16:55:07 +0900

    5.3.3 パラメータの調整[4]

--- section_particle_filter/motion_test_forward.py
+++ section_particle_filter/motion_test_forward.py
@@ -12,7 +12,7 @@ initial_pose = np.array([0, 0, 0]).T
 robots = []
 r = Robot(initial_pose, sensor=None, agent=Agent(0.1,0.0))
 
-for i in range(100):
+for i in range(10):
     copy_r = copy.copy(r)
     copy_r.distance_until_noise = copy_r.noise_pdf.rvs() # 最初に雑音が発生するタイミングを変える
     world.append(copy_r) # アニメーションの際に動くように登録
@@ -23,3 +23,10 @@ world.draw()
 poses = pd.DataFrame([[math.sqrt(r.pose[0]**2+r.pose[1]**2),r.pose[2]] for r in robots], columns=['r','theta'])
 poses.transpose() # 縦横を入れ替えて表示
 print(poses)
+
+theta_var = poses["theta"].var()
+poses_mean = poses["r"].mean()
+print(f"theta_var: {theta_var}")
+print(f"poses_mean: {poses_mean}")
+sigma_on = math.sqrt(theta_var/poses_mean) # rが1m進むあたりの角度の分散
+print(f"sigma_on: {sigma_on}")
