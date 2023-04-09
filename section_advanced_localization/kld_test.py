def065e68314402e823f1f4cca5647c066949094
shuhei23 <shuhei.toda@gmail.com>
2023-04-09 16:03:57 +0900

    7.1.2 Wilson-Hilferty変換

--- section_advanced_localization/kld_test.py
+++ section_advanced_localization/kld_test.py
@@ -3,7 +3,8 @@
 import sys
 sys.path.append('../scripts/')
 from robot import *
-from scipy.stats import norm, chi2 # norm:ガウス分布、chi2:カイ二乗分布
+from scipy.stats import norm as nd# nd:ガウス分布
+from scipy.stats import chi2 # chi2:カイ二乗分布
 
 def num(epsilon, delta, binnum):
     # 真の信念分布を表現できる最小のパーティクル数Nを計算する
@@ -22,3 +23,14 @@ axr.set_title("bin:2-100000")
 axr.plot(bs, n)
 
 plt.show()
+
+
+def num_wh(epsilon, delta, binnum):
+    dof = binnum -1
+    z = nd.ppf(1.0 - delta)
+    return math.ceil(dof/(2*epsilon)*(1.0 - 2.0/(9*dof) + math.sqrt(2.0/(9*dof))*z)**3)
+                     
+for binnum in 2,4,8,1000,10000,100000:
+    print("ビン:",binnum, "ε=0.1, δ=0.01", num(0.1, 0.01, binnum), num_wh(0.1, 0.01, binnum))
+    print("ビン:",binnum, "ε=0.5, δ=0.01", num(0.1, 0.01, binnum), num_wh(0.1, 0.01, binnum))
+    print("ビン:",binnum, "ε=0.5, δ=0.05", num(0.1, 0.01, binnum), num_wh(0.1, 0.01, binnum))
