6138a35e205cdf258daf814d963a4346b040b0fe
shuhei23 <shuhei.toda@gmail.com>
2023-05-03 15:36:49 +0900

    7.3.2

--- section_advanced_localization/reset_mcl.py
+++ section_advanced_localization/reset_mcl.py
@@ -4,28 +4,26 @@ from mcl import*
 
 class ResetMcl(Mcl):
     def __init__(self, envmap, init_pose, num, motion_noise_stds={"nn":0.19, "no":0.001, "on":0.13, "oo":0.2},\
-        distance_dev_rate=0.14, direction_dev=0.05):
+        distance_dev_rate=0.14, direction_dev=0.05, alpha_threshhold = 0.001):
         super().__init__(envmap, init_pose, num, motion_noise_stds, distance_dev_rate, direction_dev)
-        self.alphas = {} # alpha値の記録（ランドマークの観測数ごと）
-    
+        self.alpha_threshhold = alpha_threshhold
+        
+    def random_reset(self):
+        for p in self.particles:
+            p.pose = np.array([np.random.uniform(-5.0, 5.0), np.random.uniform(-5.0, 5.0), np.random.uniform(-math.pi, math.pi)]).T
+            p.weight = 1/len(self.particles)
 
     def observation_update(self, observation):
         for p in self.particles:
             p.observation_update(observation, self.map, self.distance_dev_rate, self.direction_dev)
-        
-        # alpha値の記録
-        alpha = sum([p.weight for p in self.particles]) # 式(7.28)
-        obsnum = len(observation)
-        if not obsnum in self.alphas:
-            self.alphas[obsnum] = [] # 
-                                     # 例： {"0":[0.001, 0.002], "1":[0.01, 0.002], "2":[0.1]}
-        
-        self.alphas[obsnum].append(alpha)
 
         self.set_ml()
-        self.resampling() # ここで重みの合計は1になる
+        if sum([p.weight for p in self.particles]) < self.alpha_threshhold:
+            # 異常が起きている
+            self.random_reset()
+        else:
+            self.resampling() # ここで重みの合計は1になる
         
-
 def trial():
     time_interval = 0.1
     world = World(30,time_interval, debug=False)
@@ -38,12 +36,13 @@ def trial():
     world.append(m)
 
     ### ロボットを作る
-    initial_pose = np.array([-4,-4,0]).T
+    initial_pose = np.array([0,0,0]).T
     estimator = ResetMcl(m,initial_pose,100)
     circling = EstimationAgent(time_interval, 0.2, 10.0/180*math.pi, estimator)
     
     initial_pose_robot = np.array([0,0,0]).T
-    r = Robot(initial_pose_robot, sensor=Camera(m), agent=circling, color="red")
+    #r = Robot(initial_pose_robot, sensor=Camera(m), agent=circling, color="red") #誘拐なしなのでコメントアウト
+    r = Robot(initial_pose_robot, sensor=Camera(m), agent=circling, expected_kidnap_time=10.0, color="red")
     world.append(r)
 
     ### アニメーション実行
@@ -53,6 +52,3 @@ def trial():
 
 if __name__ == '__main__':
     estimator = trial()
-    for n in estimator.alphas.keys(): # ["0", "1", "2"]
-        print("landmarks: ", n, "particles: ", len(estimator.particles), "min: ", min(estimator.alphas[n]), "max: ", max(estimator.alphas[n]))
-    
