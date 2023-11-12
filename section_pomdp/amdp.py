1bafcc1b62e3eaa0ef234d3d4332ccb578403baa
shuhei23 <shuhei.toda@gmail.com>
2023-11-12 22:49:39 +0900

    12.4

--- section_pomdp/amdp.py
+++ section_pomdp/amdp.py
@@ -4,7 +4,7 @@ sys.path.append("../scripts/")
 
 from puddle_world import np
 from dynamic_programming import *
-
+import pprint
 
 class BeliefDynamicProgramming(DynamicProgramming):
     def __init__(
@@ -14,6 +14,7 @@ class BeliefDynamicProgramming(DynamicProgramming):
         puddles,
         time_interval,
         sampling_num,
+        camera,
         puddle_coef=100,
         lowerleft=np.array([-4, -4]).T,
         upperright=np.array([4, 4]).T,
@@ -29,6 +30,9 @@ class BeliefDynamicProgramming(DynamicProgramming):
             lowerleft,
             upperright,
         )
+
+        self.actions = [(0.0, 2.0), (0.0,-2.0),(1.0, 0.0),(-1.0, 0.0)]
+        self.state_transition_probs = self.init_state_transition_probs(time_interval, sampling_num)
 
         self.index_nums = np.array(
             [*self.index_nums, len(dev_borders) + 1]
@@ -50,6 +54,32 @@ class BeliefDynamicProgramming(DynamicProgramming):
         self.motion_sigma_transition_probs = (
             self.init_motion_sigma_transition_probs()
         )  # 辞書型のデータが格納される
+        self.obs_sigma_transition_probs = self.init_obs_sigma_transition_probs(camera)
+        print("init End\n")
+
+
+    def init_obs_sigma_transition_probs(self, camera):
+        probs = {}
+        for index in self.indexes:
+            pose = self.pose_min + self.widths*(np.array(index[0:3]).T + 0.5) # セルの中心座標
+            sigma = (self.dev_borders_side[index[3]] + self.dev_borders_side[index[3]+1])/2 #範囲の真ん中の標準偏差を遷移前の状態として使う
+            S = (sigma**2)*np.eye(3) # Σ_hatの初期値、sigma:σ_hat
+
+            for d in camera.data(pose): # センサ値を繰り返しSに適用、センサ値の観測値分だけ式(12.19)を繰り返し実行してΣを更新する
+                S = self.observation_update(d[1], S, camera, pose)
+            probs[index] = {self.cov_to_index(S):1.0} 
+        
+        return probs 
+    
+    def observation_update(self, landmark_id, S, camera, pose):# 式(12.19) (12.20)、 SはΣ^
+        distance_def_rate = 0.14 # センサ値の標準偏差のパラメータ kf.pyから持ってくる
+        direction_dev = 0.05
+
+        H =matH(pose, camera.map.landmarks[landmark_id].pos)
+        estimated_z = IdealCamera.observation_function(pose, camera.map.landmarks[landmark_id].pos)
+        Q = matQ(distance_def_rate*estimated_z[0], direction_dev)
+        K = S.dot(H.T).dot(np.linalg.inv(Q + H.dot(S).dot(H.T))) #(12.20)
+        return (np.eye(3) - K.dot(H)).dot(S) #(12.19)
 
     def init_motion_sigma_transition_probs(self):
         probs = {}
@@ -151,27 +181,60 @@ class BeliefDynamicProgramming(DynamicProgramming):
             ].items():
                 # print("sigma_after, sigma_prob  = {0}, {1}".format(sigma_after, sigma_prob))
                 # print("[*after, sigma_after] = {}".format([*after, sigma_after]))
-                value += (
-                    (self.value_function[tuple([*after, sigma_after])] + reward)
-                    * prob
-                    * sigma_prob
-                )
+                for sigma_obs, sigma_obs_prob in self.obs_sigma_transition_probs[(*after, sigma_after)].items():
+                    value += (
+                        (self.value_function[tuple([*after, sigma_obs])] + reward)
+                        * prob
+                        * sigma_prob
+                        * sigma_obs_prob
+                    )
 
         return value
 
+def trial():
+    puddles = [Puddle((-2, 0), (0, 2), 0.1), Puddle((-0.5, -2), (2.5, 1), 0.1)]
+    ### 地図とカメラを作る
+    m = Map()
+    for ln in [(1,4), (4,1), (-4,1), (-2,1)]:
+        m.append_landmark(Landmark(*ln))
+    c = IdealCamera(m)
+    
+    dp = BeliefDynamicProgramming(
+        np.array([0.2, 0.2, math.pi / 18]).T, Goal(-3, -3), puddles, 0.1, 10, c
+    )
+    # delta = dp.value_iteration_sweep()
+    # print(dp.obs_sigma_transition_probs)
+    # pprint.pprint(dp.obs_sigma_transition_probs)
+    # import seaborn as sns
+    
+    # v = dp.value_function[:, :, 18, 1]  # 4番目のインデックス: エントロピーの離散区分のインデックス
+    # sns.heatmap(np.rot90(v), square=False)
+    # plt.show()
+    # v = dp.value_function[:, :, 18, 0]
+    # sns.heatmap(np.rot90(v), square=False)
+    # plt.show()
+    # print(dp.motion_sigma_transition_probs)
+    
+    delta = 1e100
+    counter = 0
 
-puddles = [Puddle((-2, 0), (0, 2), 0.1), Puddle((-0.5, -2), (2.5, 1), 0.1)]
-dp = BeliefDynamicProgramming(
-    np.array([0.2, 0.2, math.pi / 18]).T, Goal(-3, -3), puddles, 0.1, 10
-)
-delta = dp.value_iteration_sweep()
+    while delta > 0.01:
+        delta = dp.value_iteration_sweep()
+        counter += 1
+        print(counter, delta)
+        save(dp)
 
-import seaborn as sns
 
-v = dp.value_function[:, :, 18, 1]  # 4番目のインデックス: エントロピーの離散区分のインデックス
-sns.heatmap(np.rot90(v), square=False)
-plt.show()
-v = dp.value_function[:, :, 18, 0]
-sns.heatmap(np.rot90(v), square=False)
-plt.show()
-# print(dp.motion_sigma_transition_probs)
+def save(dp): # 人はこれを"関数"と呼ぶ
+    with open("policy_amdp.txt","w") as f:
+        for index in dp.indexes:
+            p = dp.policy[index]
+            f.write("{} {} {} {} {} {}\n".format(index[0], index[1], index[2], index[3], p[0],p[1]))
+
+    with open("value_amdp.txt", "w") as f:
+        for index in dp.indexes:
+            p = dp.value_function[index]
+            f.write("{} {} {} {} {}\n".format(index[0], index[1], index[2], index[3], p))
+
+if __name__ == "__main__":
+    trial()
